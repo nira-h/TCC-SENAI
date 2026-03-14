6cd2a1ab6c686396e461a82611f43329bc32565a
nira-h <nicolas.pacheco@aluno.senai.br>
2026-03-14 12:46:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Meu Site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>Meu primeiro site com PHP</h1>
+
+    <?php
+    echo "<p>Olá, mundo! PHP funcionando.</p>";
+    ?>
+
+</body>
+
+</html>
